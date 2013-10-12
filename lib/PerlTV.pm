75fcef79c2f483596f47b11988e62bca34fbc187
Gabor Szabo <szabgab@gmail.com>
2013-10-12 18:54:57 +0300

    fix the json, move the long descriptions to the data directory

--- lib/PerlTV.pm
+++ lib/PerlTV.pm
@@ -1,10 +1,42 @@
 package PerlTV;
 use Dancer2;
 
-our $VERSION = '0.1';
+our $VERSION = '0.01';
+use Cwd qw(abs_path);
+use Path::Tiny ();
+use JSON::Tiny ();
+use Data::Dumper qw(Dumper);
+
+hook before => sub {
+	my $appdir = abs_path config->{appdir};
+	my $json = JSON::Tiny->new;
+	my $data = $json->decode( Path::Tiny::path("$appdir/videos.json")->slurp_utf8 );
+	if (defined $data) {
+		set data => $data;
+	} else {
+		set error => $json->error;
+	}
+};
+
 
 get '/' => sub {
-    template 'index';
+	my $error = setting('error');
+	if ($error) {
+		warn $error;
+		return template 'error';
+	}
+
+	my $data = setting('data');
+	my $i = int rand scalar @{ $data->{videos} };
+	my $video = $data->{videos}[$i];
+	$video->{description} = '';
+	if ($video->{path}) {
+		my $path = Path::Tiny::path(abs_path(config->{appdir}) . "/data/$video->{path}");
+		if (-e $path) {
+			$video->{description} = $path->slurp_utf8;
+		}
+	}
+	template 'index', { video => $video };
 };
 
 true;
