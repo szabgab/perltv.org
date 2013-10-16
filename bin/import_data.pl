d04b0f5cd69123e15dfbfe7edf3107513ec12672
Gabor Szabo <szabgab@gmail.com>
2013-10-16 11:58:09 +0300

    add featured: field to data files and generated the featured.json

--- bin/import_data.pl
+++ bin/import_data.pl
@@ -13,6 +13,7 @@ use PerlTV::Tools qw(read_file);
 
 my $dir = path($0)->absolute->parent->parent->child('data');
 my @videos;
+my @featured;
 foreach my $f ($dir->children) {
 	my $video = read_file($f);
 	#warn Dumper $video;
@@ -21,14 +22,23 @@ foreach my $f ($dir->children) {
 		path  => $f->basename,
 	);
 
+	if ($video->{featured}) {
+		push @featured, {
+			date => $video->{featured},
+			path => $f->basename,
+		}
+	}
+
 	push @videos, \%entry;
 }
+
+@featured = sort { $b->{date} cmp $a->{date} } @featured;
 
 my $json = JSON::Tiny->new;
 my %data = (
 	_comment => 'This is a generated file, please do NOT edit directly',
 	videos => \@videos,
 );
-path('videos.json')->spew_utf8(  $json->encode(\%data) );
-
+path('videos.json')->spew_utf8( $json->encode(\%data) );
+path('featured.json')->spew_utf8( $json->encode(\@featured) );
 
