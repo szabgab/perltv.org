f92f1496f1ebacdfb659f4d0a662aedaa89b1164
Gabor Szabo <szabgab@gmail.com>
2013-10-21 21:42:52 +0300

    be able to fetch data from vimeo as well

--- bin/fetch.pl
+++ bin/fetch.pl
@@ -5,6 +5,8 @@ use 5.010;
 use WebService::GData::YouTube ();
 use Data::Dumper qw(Dumper);
 use Path::Tiny qw(path);
+use LWP::Simple qw(get);
+use JSON qw(from_json);
 
 my ($url, $file) = @ARGV;
 
@@ -15,31 +17,61 @@ die "Usage: $0 URL [FILE]\n" if not $url;
 my $u = URI->new($url);
 my $txt = '';
 my $title = '';
-if ($u->host, 'www.youtube.com') {
+if ($u->host eq 'www.youtube.com') {
 	my %form = $u->query_form;
 	my $id = $form{v} or die "Could not find id\n";
 	
 	my $yt = WebService::GData::YouTube->new();
 	my $video = $yt->get_video_by_id($id);
+	$title = $video->title;
 	
 	$txt .= "id: $id\n";
 	$txt .= "src: youtube\n";
-	$txt .= "title: " . $video->title . "\n";
+	$txt .= "title: $title\n";
 	$txt .= "speaker: \n";
-	#$txt .= "nickname: \n";
-	#$txt .= "home: \n";
 	$txt .= "source: \n";
-	$txt .= "view_count: " . $video->view_count . "\n";
-	$txt .= "favorite_count: " . $video->favorite_count . "\n";
+	$txt .= "view_count: " . ($video->view_count||0) . "\n";
+	$txt .= "favorite_count: " . ($video->favorite_count||0) . "\n";
 	my $length = seconds_to_time($video->duration);   # in seconds
 	$txt .= "length: $length\n";
 	$txt .= "format: markdown\n";
+	$txt .= "thumbnail: \n";
 	#my $keywords = $video->keywords;
 	$txt .= "\n__DESCRIPTION__\n\n";
 	$txt .= $video->description . "\n";
 
-	$title = $video->title;
+} elsif ($u->host eq 'vimeo.com') {
+	my $id = $u->path;
+	$id =~ s{^/}{};
+	die "vimeo Id is expected to be all digits. This is '$id'\n" if $id !~ /^\d+$/;
+	#die $id;
+	my $url = "http://vimeo.com/api/v2/video/$id.json";
+	my $json = get $url;
+	#die $json;
+	my $list = from_json $json;
+	my $data = $list->[0];
+	#die Dumper $data;
+    $title = $data->{title};
+
+	$txt .= "id: $id\n";
+	$txt .= "src: vimeo\n";
+	$txt .= "title: $title\n";
+    $txt .= "speaker:\n";
+	$txt .= "source: \n";
+	$txt .= "view_count: $data->{stats_number_of_plays}\n";
+	$txt .= "favorite_count: $data->{stats_number_of_likes}\n";
+	my $length = seconds_to_time($data->{duration});   # in seconds
+	$txt .= "length: $length\n";
+	$txt .= "format: markdown\n";
+	$txt .= "thumbnail: $data->{thumbnail_medium}\n";
+	$txt .= "tags: $data->{tags}}\n";
+	$txt .= "\n__DESCRIPTION__\n\n";
+	$txt .= "$data->{description}\n";
+
+} else {
+	die "Unknown host " . $u->host;
 }
+
 
 if (not $file) {
 	$file = lc $title;
