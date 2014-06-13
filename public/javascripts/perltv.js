79195643f1ab15a0c01aeb79c332cf988c83d080
Gabor Szabo <gabor@szabgab.com>
2014-06-13 17:00:44 +0300

    sort videos by date/featured/title

--- public/javascripts/perltv.js
+++ public/javascripts/perltv.js
@@ -34,6 +34,40 @@ function show(id, cls) {
 	};
 }
 
+function sort_rows() {
+	console.log('sort: ' + this.id);
+	var column = 2; // title
+	if (this.id == 'sort_date') {
+		column = 0;
+	}
+	if (this.id == 'sort_featured') {
+		column = 1;
+	}
+	if (this.id == 'sort_title') {
+		column = 2;
+	}
+
+	var ch = document.getElementById('videos').children;
+	//console.log(ch.length);
+	var html = '<li>' + ch[0].innerHTML + '</li>';
+	//console.log(html);
+
+	//var sort_descending = get_status('sort_descending');
+
+	var arr = new Array;
+	for (var i = 1; i < ch.length; i++) {
+		arr.push({
+			"field" : ch[i].children[column].innerHTML,
+			"html"  : ch[i].innerHTML
+		});
+	}
+	arr.sort(function(a, b) { return a['field'].localeCompare(b['field']) });
+	html += arr.map(function(a) { return '<li>' + a['html'] + '</li>' }).join('');
+	//console.log(html);
+	document.getElementById('videos').innerHTML = html;
+	add_listeners();
+}
+
 
 function on_load() {
 	show_date     = document.getElementById('show_date');
@@ -42,6 +76,16 @@ function on_load() {
 	show_featured = document.getElementById('show_featured');
 	show_featured.addEventListener('click', toggle);
 	show('show_featured', 'featured');
+
+	add_listeners();
+}
+function add_listeners() {
+	if (document.getElementById('sort_date') == undefined) {
+		return; // pages without lists
+	}
+	document.getElementById('sort_date').addEventListener('click', sort_rows);
+	document.getElementById('sort_featured').addEventListener('click', sort_rows);
+	document.getElementById('sort_title').addEventListener('click', sort_rows);
 }
 
 on_load();
